f2ac8502881955f594e65c3fa0fdd93244e63430
yoxi <yongxingzhao@foxmail.com>
2026-06-07 23:22:06 +0800

    Add .config/zsh/functions/yazi.zsh

---
+++ home/dot_config/zsh/functions/yazi.zsh
@@ -0,0 +1,8 @@
+function yz() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+}
