efae6da70f61207078d87af401f2c3c37794dcb4
yoxi <yongxingzhao@foxmail.com>
2026-06-13 22:14:47 +0800

    Add .config/nvim/lua/plugins/render-markdown.lua

---
+++ home/dot_config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      render_modes = { "n", "c", "t" },
+      heading = { position = "overlay", width = "full", border = true },
+      code = { style = "full", width = "block", min_width = 60, border = "thin" },
+      pipe_table = { style = "full", cell = "padded" },
+    },
+    keys = {
+      { "<leader>um", "<Cmd>RenderMarkdown toggle<CR>", desc = "Toggle Markdown Render", ft = "markdown" },
+    },
+  },
+}
